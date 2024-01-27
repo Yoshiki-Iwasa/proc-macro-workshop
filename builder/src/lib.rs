1edefb45863d251b648460a02a148d7a50b97286
Yoshiki-Iwasa <iwasayoshiki222@gmail.com>
2024-01-27 16:01:52 +0900

    pass builder/02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,18 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let name = input.ident;
+
+    let expand = quote! {
+        impl #name {
+            pub fn builder() {}
+        }
+    };
+
+    proc_macro::TokenStream::from(expand)
 }
