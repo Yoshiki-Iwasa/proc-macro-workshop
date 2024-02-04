295b296efc1160a1387065c55ec7b49fc41d3177
Yoshiki-Iwasa <iwasayoshiki222@gmail.com>
2024-02-04 19:59:26 +0900

    pass debug/01

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,15 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    expand(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
 
-    unimplemented!()
+fn expand(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    Ok(quote! {})
 }
