7273b52df1b1d34d652f0bb8de3e592fefe99cfe
Yoshiki-Iwasa <iwasayoshiki222@gmail.com>
2024-02-15 18:48:39 +0900

    pass debug/03

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, Data, DeriveInput, Error, Field, LitStr};
+use syn::{parse_macro_input, Data, DeriveInput, Error, Expr, Field, LitStr};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     expand(input)
@@ -24,9 +24,16 @@ fn expand(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
                 || Err(syn_error("Field name is expected", field)),
                 syn::Result::Ok,
             )?;
+            let format = extract_debug_attr(field);
             let field_name_literal = LitStr::new(ident.to_string().as_str(), Span::call_site());
+
+            let value = if let Some(fmt) = format {
+                quote!(&format_args!(#fmt, &self.#ident))
+            } else {
+                quote!(&self.#ident)
+            };
             Ok(quote! {
-                .field(#field_name_literal, &self.#ident)
+                .field(#field_name_literal, #value)
             })
         })
         .collect::<syn::Result<Vec<_>>>()?;
@@ -39,6 +46,26 @@ fn expand(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
             }
         }
     })
+}
+
+fn extract_debug_attr(field: &Field) -> Option<String> {
+    for attr in &field.attrs {
+        let meta_name_value = attr.meta.require_name_value().unwrap();
+
+        if !meta_name_value.path.is_ident("debug") {
+            continue;
+        }
+
+        if let Expr::Lit(lit) = &meta_name_value.value {
+            match &lit.lit {
+                syn::Lit::Str(s) => return Some(s.value()),
+                _ => continue,
+            }
+        } else {
+            continue;
+        }
+    }
+    None
 }
 
 fn extract_fields(input: &DeriveInput) -> syn::Result<Vec<Field>> {
--- debug/tests/ext.rs
+++ debug/tests/ext.rs
@@ -3,9 +3,9 @@ use derive_debug::CustomDebug;
 #[derive(CustomDebug)]
 pub struct Field {
     name: &'static str,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
-
 fn main() {
     let f = Field {
         name: "F",
