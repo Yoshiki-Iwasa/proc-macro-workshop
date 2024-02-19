e8e93aff15615228da06c5af0d56f95b00228ee0
Yoshiki-Iwasa <iwasayoshiki222@gmail.com>
2024-02-20 07:45:52 +0900

    pass sort/01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,16 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
 
-    unimplemented!()
+    let input: Item = parse_macro_input!(input);
+
+    let token_steram = quote! {
+        #input
+    };
+
+    token_steram.into()
 }
