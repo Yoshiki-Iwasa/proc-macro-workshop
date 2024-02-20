ad97dd461a8853af4fbbde6e082ef63f62415b7d
Yoshiki-Iwasa <iwasayoshiki222@gmail.com>
2024-02-20 09:10:01 +0900

    pass sorted/03

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,6 +1,8 @@
+use std::cmp::Ordering;
+
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, Error, Item};
+use syn::{parse_macro_input, spanned::Spanned, Error, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(
@@ -15,11 +17,30 @@ pub fn sorted(
 }
 
 fn check_sorted(input: Item) -> syn::Result<proc_macro2::TokenStream> {
-    let Item::Enum(_) = &input else {
+    let Item::Enum(item_enum) = &input else {
         return Err(Error::new(
             Span::call_site(),
             "expected enum or match expression",
         ));
+    };
+
+    let variants = &item_enum.variants;
+
+    // 辞書順のどこにいれるべきかを判定しないといけない
+    //
+    if let Some(Err(e)) = variants.iter().map(syn::Result::Ok).reduce(|prev, now| {
+        let prev = prev?;
+        let now = now?;
+
+        if prev.ident.cmp(&now.ident) == Ordering::Greater {
+            return Err(syn::Error::new(
+                now.span(),
+                format!("{} should sort before {}", now.ident, prev.ident),
+            ));
+        }
+        Ok(prev)
+    }) {
+        return Err(e);
     };
 
     let token_stream = quote! {
